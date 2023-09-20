c9e1507986ed831c7671566807417b5d9f6877c2
cassi <admin@cassi.top>
2023-09-20 18:54:33 +0800

    init

---
+++ main.py
@@ -0,0 +1,99 @@
+import datetime
+import json
+from json import JSONDecodeError
+import paho.mqtt.client as mqtt
+import cx_Oracle
+
+
+# 定义回调函数来处理连接成功的事件
+def on_connect(c, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+
+    for topic in topic_list:
+        client.subscribe(topic)
+
+
+# 定义回调函数来处理收到消息的事件
+def on_message(c, userdata, msg):
+    try:
+        payload_dict = json.loads(msg.payload)
+    except JSONDecodeError as e:
+        print(e)
+        return
+
+    if msg.topic == "/smartpower/status":
+        handle_status_message(payload_dict)
+    if msg.topic == "/smartpower/systeminfo":
+        pass
+    print("Received message '" + str(msg.payload) + "' on topic '" + msg.topic + "'")
+
+
+def handle_status_message(d: dict):
+    k = d.keys()
+    bind_str = [":"+str(x) for x in range(len(k)+2)]
+    v = list(d.values()) + [datetime.datetime.now(), datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")]
+    # 写入 status
+    status_sql = "INSERT INTO SMARTPLUG.DEVICE_STATUS(" + (",".join(k)).upper() + ", INSERT_TIME, ITIME) VALUES (" + \
+                 ','.join(bind_str) + ")"
+    # 写入status_first
+    delete_device_first = "DELETE FROM SMARTPLUG.DEVICE_STATUS_FIRST WHERE DEVICEID=:1"
+    status_first_sql = "INSERT INTO SMARTPLUG.DEVICE_STATUS_FIRST(" + (",".join(k)).upper() + ", INSERT_TIME, ITIME) \
+                    VALUES (" + ','.join(bind_str) + ")"
+    conn = pool_oracle.acquire()
+    cursor = conn.cursor()
+    try:
+        cursor.execute(status_sql, v)
+        cursor.execute(delete_device_first, d.get("DeviceID"))
+        cursor.execute(status_first_sql)
+        conn.commit()
+    except Exception as e:
+        print(e)
+    finally:
+        pool_oracle.release(conn)
+
+
+def handle_systeminfo_message(d: dict):
+    device_info_sql = "INSERT INTO SMARTPLUG.DEVICE_INFO(DEVICEID, PROJECT, TYPE, OTP_VALUE, TIMEZONE, FW_VERSION, " \
+                      "MAX_CURRENT_STATUS, MAX_CURRENT_VALUE, ACOUTAUTORECOVER, CO_VALUE, OVER_TEMP, INSERTTIME)" \
+                      "VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12)"
+    device_value = [d.get("DeviceID"), d.get("Project"), d.get("Type"), d.get("OTP_Value"), d.get("TimeZone"),
+                    d.get("FwVersion"), d.get("MaxCurrentStatus"), d.get("MaxCurrentValue"), d.get("AcOutAutoRecover"),
+                    d.get("CO_Value"), d.get("OVER_TEMP"), datetime.datetime.now()]
+    conn = pool_oracle.acquire()
+    cursor = conn.cursor()
+    try:
+        cursor.execute(device_info_sql, device_value)
+        conn.commit()
+    except Exception as e:
+        print(e)
+    finally:
+        pool_oracle.close()
+
+
+if __name__ == "__main__":
+    topic_list = [
+        "/smartpower/status",
+        "/smartpower/systeminfo"
+    ]
+    # cx_oracle 连接池
+    pool_oracle = cx_Oracle.SessionPool(
+        user="SMARTPLUG", password="ABB", dsn="localhost:1521/orclpdb", min=10, max=10, increment=0, threaded=True,
+        encoding="UTF-8")
+
+    # 创建MQTT客户端实例
+    client = mqtt.Client()
+
+    # 设置连接回调函数
+    client.on_connect = on_connect
+
+    # 设置消息接收回调函数
+    client.on_message = on_message
+
+    try:
+        # 连接到MQTT服务器
+        client.connect("mlbaeabbserver.cn", 1883, 60)
+
+        # 开始循环处理网络流量和调用回调函数
+        client.loop_forever()
+    except KeyboardInterrupt:
+        pool_oracle.close()
---
+++ mqtt_service.py
@@ -0,0 +1,68 @@
+import win32serviceutil
+import win32service
+import win32event
+import servicemanager
+import socket
+import os
+import sys
+import logging
+from logging.handlers import TimedRotatingFileHandler
+from paho.mqtt
+
+
+class MyService(win32serviceutil.ServiceFramework):
+    _svc_name_ = 'SmartPowerMqttService'
+    _svc_display_name_ = 'Smart Power Handle Mqtt Service'
+
+    def __init__(self, args):
+        win32serviceutil.ServiceFramework.__init__(self, args)
+        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
+        socket.setdefaulttimeout(60)
+        self.is_alive = True
+
+        # 设置日志记录器
+        self._setup_logging()
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
+        self.is_alive = False
+
+    def SvcDoRun(self):
+        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
+                              servicemanager.PYS_SERVICE_STARTED,
+                              (self._svc_name_, ''))
+        self.main()
+
+    def _setup_logging(self):
+        log_filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'my_service.log')
+        log_handler = TimedRotatingFileHandler(log_filename, when='midnight', interval=1, backupCount=7)
+        log_handler.suffix = '%Y-%m-%d.log'
+        log_formatter = logging.Formatter('%(asctime)s [%(levelname)s]: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        log_handler.setFormatter(log_formatter)
+
+        self.logger = logging.getLogger()
+        self.logger.addHandler(log_handler)
+        self.logger.setLevel(logging.INFO)
+
+    def main(self):
+        # 在这里放置你的Python服务代码，使用self.logger记录日志
+        self.logger.info('Service started.')
+        try:
+            while self.is_alive:
+                # 你的服务逻辑
+                pass
+        except Exception as e:
+            self.logger.error(f'An error occurred: {str(e)}')
+            # 处理错误
+
+        self.logger.info('Service stopped.')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        servicemanager.Initialize()
+        servicemanager.PrepareToHostSingle(MyService)
+        servicemanager.StartServiceCtrlDispatcher()
+    else:
+        win32serviceutil.HandleCommandLine(MyService)
